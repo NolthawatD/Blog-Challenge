35df0bf935374ff0f12f26a1033f15f3d38e8153
Nolthawat Deangharn <nolthawat.d@gmail.com>
2024-06-28 21:03:12 +0700

    [New] Search bar

--- blog-frontend/src/app/page.tsx
+++ blog-frontend/src/app/page.tsx
@@ -3,12 +3,9 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<div>
-			<form className="max-w-md mx-auto">
-				<label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
-					Search
-				</label>
+			<div className="my-6 ">
 				<div className="relative">
-					<div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+					<div className="absolute inset-y-0 start-0 flex items-center ps-3  pointer-events-none">
 						<svg
 							className="w-4 h-4 text-gray-500 dark:text-gray-400"
 							aria-hidden="true"
@@ -28,19 +25,19 @@ export default function Home() {
 					<input
 						type="search"
 						id="default-search"
-						className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-						placeholder="Search Mockups, Logos..."
+						className="block w-full p-4 ps-10 bg-custom-page-bg text-sm text-gray-700 border border-gray-300 rounded-lg"
+						placeholder="Search"
 						required
 					/>
-					<button
-						type="submit"
-						className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-					>
-						Search
-					</button>
 				</div>
-			</form>
-			{/* // content */}
+				<button
+					type="submit"
+					className="text-white absolute end-2.5 bottom-2.5 bg-custom-success 0focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 "
+				>
+					Create +
+				</button>
+			</div>
+			{/* content */}
 			<div className="bg-white rounded-lg shadow-md p-8">
 				<div className="container mx-auto">
 					<div className="flex flex-row flex-wrap py-4">
