196b290c3b496fc34e9bd52c8b146e49a4417d29
Nolthawat Deangharn <nolthawat.d@gmail.com>
2024-06-30 17:11:27 +0700

    Update page.tsx

--- blog-frontend/src/app/(landing-page)/blog/page.tsx
+++ blog-frontend/src/app/(landing-page)/blog/page.tsx
@@ -7,13 +7,8 @@ import { useQuery } from "react-query";
 
 export default function Home() {
 	const [dropdownOpen, setDropdownOpen] = useState(false);
-	const [selectedItem, setSelectedItem] = useState<string | null>(null);
 	const [isSearchFocused, setIsSearchFocused] = useState(false);
 	const [searchInput, setSearchInput] = useState("");
-
-	const handleItemClick = (item: string) => {
-		setSelectedItem(item);
-	};
 
 	const toggleDropdown = () => {
 		setDropdownOpen(!dropdownOpen);
@@ -32,16 +27,22 @@ export default function Home() {
 
 	const toggleDropdownModal = () => setDropdownOpenModal(!dropdownOpen);
 
-	const handleCommunitySelect = (community: string) => {
-		setSelectedCommunity(community);
-		setDropdownOpenModal(false);
-	};
 
 	const [blogs, setBlogs] = useState([]);
 	const [communities, setCommunities] = useState([]);
 	const [communitySelect, setCommunitySelect] = useState<number[]>([]);
 
-	const handleCommunity = (comId: number) => {
+	const [toggleModalCreate, setToggleModalCreate] = useState(false);
+	const [communityIdCreate, setCommunityIdCreate] = useState<number>(0);
+	const [communityNameCreate, setCommunityNameCreate] = useState<string>("");
+
+	const handleCommunityCreate = (communityId: number, communityName: string) => {
+		setCommunityIdCreate(communityId);
+		setCommunityNameCreate(communityName)
+		setDropdownOpenModal(false);
+	};
+
+	const handleCommunityQuery = (comId: number) => {
 		setCommunitySelect((prevCommunitySelect) => {
 			if (prevCommunitySelect.includes(comId)) {
 				return prevCommunitySelect.filter((id) => id !== comId);
@@ -125,6 +126,7 @@ export default function Home() {
 								onBlur={handleSearchBlur}
 								autoFocus
 								onChange={(e: any) => setSearchInput(e.target.value)}
+								value={searchInput}
 							/>
 						)}
 
@@ -172,7 +174,7 @@ export default function Home() {
 										{communities?.map((community: any) => (
 											<li className="flex items-center justify-between" key={community.name}>
 												<button
-													onClick={() => handleCommunity(community.id)}
+													onClick={() => handleCommunityQuery(community.id)}
 													className={`block px-4 py-2 items-center ${
 														communitySelect.includes(community.id) ? "font-medium text-black" : ""
 													}`}
@@ -187,7 +189,11 @@ export default function Home() {
 							</div>
 
 							{/* Create */}
-							<button type="submit" className="text-white bg-custom-success font-medium rounded-lg text-sm px-6 py-3">
+							<button
+								type="submit"
+								className="text-white bg-custom-success font-medium rounded-lg text-sm px-6 py-3"
+								onClick={() => setToggleModalCreate(!toggleModalCreate)}
+							>
 								Create+
 							</button>
 						</>
@@ -234,120 +240,123 @@ export default function Home() {
 				</div>
 			</div>
 
-			<div className="hidden">
-				{/* <!-- Backdrop --> */}
-				<div id="modal-backdrop" className="fixed inset-0 bg-black bg-opacity-50"></div>
+			{toggleModalCreate && (
+				<div className="">
+					{/* <!-- Backdrop --> */}
+					<div id="modal-backdrop" className="fixed inset-0 bg-black bg-opacity-50"></div>
 
-				{/* <!-- Main modal --> */}
-				<div
-					id="crud-modal"
-					tabIndex={-1}
-					aria-hidden="true"
-					className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 w-full max-w-3xl "
-				>
-					<div className="relative bg-white rounded-lg shadow-xl sm:mx-0 mx-4">
-						{/* <!-- Modal header --> */}
-						<div className="flex items-center justify-between p-6 border-b">
-							<h3 className="text-xl font-semibold text-gray-900">Create Post</h3>
-							<button
-								type="button"
-								className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center"
-								data-modal-toggle="crud-modal"
-							>
-								<svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-									<path
-										stroke="currentColor"
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										strokeWidth="2"
-										d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-									/>
-								</svg>
-							</button>
-						</div>
-
-						<form className="p-6">
-							{/* Community selection dropdown */}
-							<div className="mb-4">
+					{/* <!-- Main modal --> */}
+					<div
+						id="crud-modal"
+						tabIndex={-1}
+						aria-hidden="true"
+						className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 w-full max-w-3xl "
+					>
+						<div className="relative bg-white rounded-lg shadow-xl sm:mx-0 mx-4">
+							{/* <!-- Modal header --> */}
+							<div className="flex items-center justify-between p-6 border-b">
+								<h3 className="text-xl font-semibold text-gray-900">Create Post</h3>
 								<button
-									id="communityDropdown"
-									onClick={toggleDropdownModal}
 									type="button"
-									className="w-full md:w-auto bg-white border border-custom-success text-custom-success hover:bg-green-50 font-medium rounded-md text-sm px-4 py-2.5 text-left inline-flex items-center justify-between"
+									className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center"
+									data-modal-toggle="crud-modal"
+									onClick={() => setToggleModalCreate(!toggleModalCreate)}
 								>
-									{selectedCommunity || "Choose a community"}
-									<svg
-										className="w-4 h-4 ml-2"
-										fill="none"
-										stroke="currentColor"
-										viewBox="0 0 24 24"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+									<svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+										<path
+											stroke="currentColor"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth="2"
+											d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+										/>
 									</svg>
 								</button>
-
-								{dropdownOpenModal && (
-									<div className="relative">
-										<ul className="absolute z-10 w-full py-2 mt-1 bg-white rounded-md shadow-lg">
-											{communities.map((community) => (
-												<li key={community}>
-													<button
-														onClick={() => handleCommunitySelect(community)}
-														className={`flex justify-between items-center w-full px-4 py-2 text-sm text-left text-gray-700 hover:bg-gray-100 ${
-															selectedCommunity === community ? "bg-green-50" : ""
-														}`}
-													>
-														<span>{community}</span>
-														{selectedCommunity === community && (
-															<svg className="w-4 h-4" viewBox="0 0 20 20" fill="currentColor">
-																<path
-																	fillRule="evenodd"
-																	d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-																	clipRule="evenodd"
-																/>
-															</svg>
-														)}
-													</button>
-												</li>
-											))}
-										</ul>
-									</div>
-								)}
 							</div>
 
-							<input
-								id="title"
-								className="block p-2.5 mb-4 w-full text-sm text-gray-900 bg-white rounded-md border border-gray-300 focus:ring-green-500 focus:border-green-500"
-								placeholder="Title"
-							/>
-
-							<textarea
-								id="description"
-								rows={10}
-								className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-md border border-gray-300 focus:ring-green-500 focus:border-green-500"
-								placeholder="What's on your mind..."
-							></textarea>
-
-							<div className="flex flex-col-reverse sm:flex-row sm:justify-end items-center space-y-4 space-y-reverse sm:space-y-0 sm:space-x-4 mt-6">
-								<div className="w-80 sm:w-auto">
+							<form className="p-6">
+								{/* Community selection dropdown */}
+								<div className="mb-4">
 									<button
+										id="communityDropdown"
+										onClick={toggleDropdownModal}
 										type="button"
-										className="w-full sm:w-32 px-4 sm:px-6 py-2 rounded-md border border-custom-success text-custom-success"
+										className="w-full md:w-auto bg-white border border-custom-success text-custom-success hover:bg-green-50 font-medium rounded-md text-sm px-4 py-2.5 text-left inline-flex items-center justify-between"
 									>
-										Cancel
+										{ communityNameCreate || "Choose a community"}
+										<svg
+											className="w-4 h-4 ml-2"
+											fill="none"
+											stroke="currentColor"
+											viewBox="0 0 24 24"
+											xmlns="http://www.w3.org/2000/svg"
+										>
+											<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+										</svg>
 									</button>
+
+									{dropdownOpenModal && (
+										<div className="relative">
+											<ul className="absolute z-10 w-full py-2 mt-1 bg-white rounded-md shadow-lg">
+												{communities.map((community: any) => (
+													<li key={community.name + "create post"}>
+														<button
+															onClick={() => handleCommunityCreate(community.id, community.name)}
+															className={`flex justify-between items-center w-full px-4 py-2 text-sm text-left text-gray-700 hover:bg-gray-100 ${
+																communityIdCreate === community.id ? "bg-green-50" : ""
+															}`}
+														>
+															<span>{community.name}</span>
+															{communityIdCreate === community.id  && (
+																<svg className="w-4 h-4" viewBox="0 0 20 20" fill="currentColor">
+																	<path
+																		fillRule="evenodd"
+																		d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+																		clipRule="evenodd"
+																	/>
+																</svg>
+															)}
+														</button>
+													</li>
+												))}
+											</ul>
+										</div>
+									)}
 								</div>
-								<div className="w-80 sm:w-auto">
-									<button type="submit" className="w-full sm:w-32 px-4 sm:px-6 py-2 bg-custom-success text-white rounded-md">
-										Post
-									</button>
+
+								<input
+									id="title"
+									className="block p-2.5 mb-4 w-full text-sm text-gray-900 bg-white rounded-md border border-gray-300 focus:ring-green-500 focus:border-green-500"
+									placeholder="Title"
+								/>
+
+								<textarea
+									id="description"
+									rows={10}
+									className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-md border border-gray-300 focus:ring-green-500 focus:border-green-500"
+									placeholder="What's on your mind..."
+								></textarea>
+
+								<div className="flex flex-col-reverse sm:flex-row sm:justify-end items-center space-y-4 space-y-reverse sm:space-y-0 sm:space-x-4 mt-6">
+									<div className="w-80 sm:w-auto">
+										<button
+											type="button"
+											className="w-full sm:w-32 px-4 sm:px-6 py-2 rounded-md border border-custom-success text-custom-success"
+										>
+											Cancel
+										</button>
+									</div>
+									<div className="w-80 sm:w-auto">
+										<button type="submit" className="w-full sm:w-32 px-4 sm:px-6 py-2 bg-custom-success text-white rounded-md">
+											Post
+										</button>
+									</div>
 								</div>
-							</div>
-						</form>
+							</form>
+						</div>
 					</div>
 				</div>
-			</div>
+			)}
 		</div>
 	);
 }
