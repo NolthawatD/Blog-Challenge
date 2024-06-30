722293b2f558893b3a969575f11c76956d6457ef
Nolthawat Deangharn <nolthawat.d@gmail.com>
2024-06-30 11:44:29 +0700

    [Fix] Modal reverse

--- blog-frontend/src/app/(landing-page)/our-blog/page.tsx
+++ blog-frontend/src/app/(landing-page)/our-blog/page.tsx
@@ -159,7 +159,7 @@ export default function Home() {
 					<div className="relative bg-white rounded-lg shadow-xl sm:mx-0 mx-4">
 						{/* <!-- Modal header --> */}
 						<div className="flex items-center justify-between p-6 border-b">
-							<h3 className="text-xl font-semibold text-gray-900">Create Post</h3>
+							<h3 className="text-xl font-semibold text-gray-900">Edit Post</h3>
 							<button
 								type="button"
 								className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center"
@@ -259,6 +259,49 @@ export default function Home() {
 					</div>
 				</div>
 			</div>
+
+			{/* <!-- Confirm Modal --> */}
+
+			<div className="hidden">
+				{/* <!-- Backdrop --> */}
+				<div id="modal-backdrop" className="fixed inset-0 bg-black bg-opacity-50"></div>
+
+				{/* <!-- Main modal --> */}
+				<div
+					id="crud-modal"
+					tabIndex={-1}
+					aria-hidden="true"
+					className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 w-full max-w-sm"
+				>
+					<div className="relative bg-white rounded-lg shadow-xl sm:mx-0 mx-4">
+						{/* Modal header */}
+						<div className="pt-6">
+							<h3 className="tracking-tighter text-2xl text-center font-medium text-black md:text-xl dark:text-gray-400 pb-3 md:mx-14">
+								Please confirm if you wish to delete the post
+							</h3>
+							<p className="tracking-tighter text-gray-500 text-center md:text-lg dark:text-gray-400 md:mx-5">
+								Are you sure you want to delete the post? Once deleted, it cannot be recovered.
+							</p>
+						</div>
+						<div className="pb-6">
+							<div className="flex flex-col-reverse sm:flex-row justify-center items-center space-y-4 space-y-reverse sm:space-y-0 sm:space-x-4 mt-6">
+								<div className="w-80 sm:w-auto">
+									<button type="button" className="w-full sm:w-32 px-4 sm:px-6 py-2 border text-gray-500 rounded-md">
+										Cancel
+									</button>
+								</div>
+								<div className="w-80 sm:w-auto">
+									<button type="submit" className="w-full sm:w-32 px-4 sm:px-6 py-2 bg-red-500 text-white rounded-md">
+										Delete
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			{/* <!-- End Confirm Modal --> */}
 		</div>
 	);
 }
