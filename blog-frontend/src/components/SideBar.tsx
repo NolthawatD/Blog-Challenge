f8cddea893a811fdd8c9b6de900b8614b560977d
Nolthawat Deangharn <nolthawat.d@gmail.com>
2024-06-30 18:13:04 +0700

    Update SideBar.tsx

--- blog-frontend/src/components/SideBar.tsx
+++ blog-frontend/src/components/SideBar.tsx
@@ -1,19 +1,22 @@
+"use client"
+
 import Image from "next/image";
 import React from "react";
+import { useRouter } from "next/navigation";
 
 function Sidebar() {
+	const router = useRouter();
 	return (
 		<div>
 			<div className="p-6 w-1/2 h-screen z-0 fixed top-0 -left-96 lg:left-0 lg:w-60  peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
 				<div className="flex flex-col justify-start item-center">
 					<div className=" mt-20 pb-4 ">
-						<div className="flex mb-2 justify-start items-center gap-4 pl-5 p-2 cursor-pointer m-auto">
+						<div className="flex mb-2 justify-start items-center gap-4 pl-5 p-2 cursor-pointer m-auto" onClick={() => router.push('/blog')}>
 							<Image width={25} height={25} alt="icon" src="/assets/image/sidebar-home.svg" />
 							<h3 className="text-base text-gray-800 hover:text-white font-semibold ">Home</h3>
 						</div>
-						<div className="flex mb-2 justify-start items-center gap-4 pl-5 p-2 cursor-pointer m-auto group-hover:text-white">
+						<div className="flex mb-2 justify-start items-center gap-4 pl-5 p-2 cursor-pointer m-auto group-hover:text-white" onClick={() => router.push('/our-blog')}>
 							<Image width={25} height={25} alt="icon" src="/assets/image/sidebar-our-blog.svg" />
-
 							<h3 className="text-base text-gray-800 font-semibold ">Our Blog</h3>
 						</div>
 					</div>
