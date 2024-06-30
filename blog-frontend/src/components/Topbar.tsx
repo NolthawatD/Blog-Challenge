9ec2d07e4f83e7e70188ad0a6abfe8bd383c2ce2
Nolthawat Deangharn <nolthawat.d@gmail.com>
2024-06-30 12:07:54 +0700

    Modify Topbar

--- blog-frontend/src/components/Topbar.tsx
+++ blog-frontend/src/components/Topbar.tsx
@@ -14,8 +14,9 @@ const Topbar = () => {
 	return (
 		<nav className="bg-green-500 p-4 fixed left-0 w-full z-10">
 			<div className="flex items-center justify-between">
-				<div className="text-white text-2xl font-bold">a Board</div>
-
+				<div className="text-white text-2xl font-bold">
+					<Image width={80} height={25} alt="icon" src="/assets/image/a-board.svg" />
+				</div>
 				<div>
 					<div className="py-2 px-8 bg-custom-success hover:bg-green-300 rounded-md group cursor-pointer hover:shadow-lg hidden md:block mb">
 						<h3 className="text-white font-semibold">Sign in</h3>
@@ -45,21 +46,21 @@ const Topbar = () => {
 				}`}
 			>
 				<button className="absolute top-4 left-4 text-white mt-10" onClick={toggleMenu}>
-					<Image width={25} height={25} alt="icon" src="/assets/image/ribbon-arrow.svg" />
+					<Image width={20} height={25} alt="icon" src="/assets/image/ribbon-arrow.svg" />
 				</button>
 				<ul className="py-2 text-sm text-white dark:text-gray-200 mt-10 mx-3" aria-labelledby="dropdownDefaultButton">
 					<li className="flex items-center justify-between mt-16">
 						{/* Add home icon in front of Dashboard button */}
 						<button className={`flex px-4 py-3  items-center ${true ? "font-medium text-white" : ""}`}>
-							<Image width={25} height={25} alt="icon" src="/assets/image/ribbon-home.svg" />
-							<span className="ml-2">Home</span>
+							<Image width={20} height={25} alt="icon" src="/assets/image/ribbon-home.svg" />
+							<span className="ml-3">Home</span>
 						</button>
 					</li>
 					<li className="flex items-center justify-between">
 						{/* Add pencil icon in front of Settings button */}
 						<button className={`flex px-4 py-3  items-center ${true ? "font-medium text-white" : ""}`}>
-							<Image width={25} height={25} alt="icon" src="/assets/image/ribbon-blog.svg"/>
-							<span className="ml-2">Our Blog</span>
+							<Image width={25} height={25} alt="icon" src="/assets/image/ribbon-blog.svg" />
+							<span className="ml-3">Our Blog</span>
 						</button>
 					</li>
 				</ul>
